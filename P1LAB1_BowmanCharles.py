649a9f1ad442036427913a493530adfe24318375
Bowmanc9009 <bowmanc9009@student.faytechcc.edu>
2025-09-15 11:44:21 -0400

    Add files via upload

---
+++ P1LAB1_BowmanCharles.py
@@ -0,0 +1,11 @@
+# Charles Bowman
+# 9/15/2025
+# P1LAB1
+# Ask the user to input first and last name and display welcome message
+
+# get first and last names from user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display message to user
+print("Hello,", first_name, last_name+"!", "Welcome to CTI-110." )
